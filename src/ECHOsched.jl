aa4c2747cdf06c5b493eab73fe158774c84b1167
petedmccallum <49063588+petedmccallum@users.noreply.github.com>
2020-08-03 21:19:57 +0100

    Main module setup

--- src/ECHOsched.jl
+++ src/ECHOsched.jl
@@ -1,9 +1,12 @@
 module ECHOsched
 
-using DataFrames, Dates, CSV, StatsBase, util
+using DataFrames, Dates, CSV, StatsBase, Statistics, Clustering # PUBLIC
+using util # DEV
 
 include("structs.jl")
 include("gatherData.jl")
+# include("extractSchedules.jl")
+include("dataFixes.jl")
 
 
 # Write your package code here.
