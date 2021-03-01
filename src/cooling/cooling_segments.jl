656dd6342876068b885da1fbc61103b691a4ca22
petedmccallum <p.mccallum@hac.uk>
2021-03-01 07:54:30 +0000

    Ranges from vec util function

--- src/cooling/cooling_segments.jl
+++ src/cooling/cooling_segments.jl
@@ -70,3 +70,15 @@ function cooling_onoff_segmentation(
     end
     return cooling_ranges
 end
+
+
+function ranges_from_vec(i)
+    sort!(i)
+    i_incremental = findall(diff(i).==1)
+    i_new_seq = findall(diff(i_incremental).!=1)
+
+    i_start = vcat(i[1],i[i_incremental[i_new_seq.+1]])
+    i_stop = vcat(i[i_incremental[i_new_seq].+1],i[end])
+    range_arr(start,stop) = start:stop
+    ranges = range_arr.(i_start,i_stop)
+end
